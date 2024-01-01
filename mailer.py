c007c732723aad2fc5c5ff6d2b1a392897154e95
Prateek Thakur <prateekthakur272@gmail.com>
2024-01-02 01:51:25 +0530

    send mail using fastapi

--- mailer.py
+++ mailer.py
@@ -1,4 +1,31 @@
-HOST = ''
-USERNAME = ''
-PASSWORD = ''
-PORT = ''
+from dotenv import dotenv_values
+from ssl import create_default_context
+from email.mime.text import MIMEText
+from email.message import EmailMessage
+from smtplib import SMTP_SSL
+
+
+env_values = dotenv_values('.env')
+
+# add the following to .env file
+# HOST = ''
+# USERNAME = ''
+# PASSWORD = ''
+# PORT = ''
+
+HOST = env_values['HOST']
+USERNAME = env_values['USERNAME']
+PASSWORD = env_values['PASSWORD']
+PORT = env_values['PORT']
+
+async def send_mail(subject:str, body:str, to:str): 
+    # create email
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['From'] = USERNAME
+    msg['To'] = to
+    msg.set_content(body)
+    # send email
+    with SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(USERNAME, PASSWORD)
+        smtp.send_message(msg)
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 import uvicorn
+from mailer import send_mail
 
 app = FastAPI(title='Mail API')
 
 @app.get('/')
 def index():
     return {'message':'FastAPI Mail'}
+
+@app.post('/mail')
+async def mail(to = Form(), subject = Form(), body = Form()):
+    await send_mail(to=to, subject=subject, body=body)
+    return {'message':'done'}
 
 
 if __name__ == '__main__':
